4f770ccd818652ece382a4bda1fa46d31844e48d
capmontulet <108439458+capmontulet@users.noreply.github.com>
2022-06-29 20:29:23 -0400

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,89 @@
+/**
+ * CET - CS Academic Level 3
+ * Declaration: I declare that this is my own original work and is free from Plagiarism
+ * This class contains the dynamically allocated array and it's processing
+ * Student Name: Thomas Stanley
+ * Student Number: 41037136      
+ * Section #: 301 
+ * Course: CST8130 - Data Structures
+ * Professor: James Mwangi PhD. 
+ * 
+ */
+public class Lab1 {
+	
+	/**
+	 * This method prints a main menu to the user, that exits once the corresponding option is selected.
+	 */
+	private static void displayMainMenu() {
+		System.out.println("Please select one of the following:");
+		System.out.println("1. Initialise a default array");
+		System.out.println("2. To specify the max size of the array");
+		System.out.println("3. Add value to an array");
+		System.out.println("4. Display values in the array");
+		System.out.println("5. Display average of the values, minumum value, maximum value, maximum modulo minimum");
+		System.out.println("6. To Exit");
+	}
+
+	/**
+	 * @param args for main method
+	 */
+	public static void main(String[] args) {
+		
+		//ensuring only numeric values are entered for whole menu
+		while(true) {
+			try {
+				
+				
+		//creating new Numbers object
+		Numbers numbers = new Numbers();
+		//creating loop condition for main menu
+		boolean loop = true;
+		
+		
+		
+		//start of menu
+		while(loop) {
+			displayMainMenu();
+			int option = numbers.keyboard.nextInt();
+			
+			//switch case for menu
+			switch(option) {
+			case 1:
+				numbers = new Numbers();
+				break;
+			case 2:
+				System.out.println("Enter new size of array: ");
+				//user input is taken for array size
+				int numbersLength = numbers.keyboard.nextInt();
+				numbers = new Numbers(numbersLength);
+				break;
+			case 3:
+				numbers.addValue(numbers.keyboard);
+				break;
+			case 4:
+				System.out.println("Numbers are: \n" + numbers.toString());
+				break;
+			case 5: 
+				numbers.findMinMax();
+				break;
+			case 6:
+				System.out.println("Exiting...");
+				numbers.keyboard.close();
+				//loop condition is broken and is exited
+				loop = false;
+				break;
+				default:
+				
+			}//end of switch
+		}//end of menu loop
+		
+		}catch(Exception e) {
+			System.out.println("Please enter an integer value");
+		}
+			
+		}//end of try catch
+		
+		
+	}
+
+}
---
+++ Numbers.java
@@ -0,0 +1,125 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * CET - CS Academic Level 3
+ * Declaration: I declare that this is my own original work and is free from Plagiarism
+ * This class contains the dynamically allocated array and it's processing
+ * Student Name: Thomas Stanley
+ * Student Number: 41037136  
+ * Section #: 301
+ * Course: CST8130 - Data Structures
+ * Professor: James Mwangi PhD. 
+ * 
+ */
+public class Numbers {
+	
+	//creates scanner object
+	Scanner keyboard = new Scanner(System.in);
+	
+	
+	/**
+	 * Stores Float values.
+	 */
+	private Float [] numbers;
+	
+	/**
+	 * Store the number of items currently in the array.
+	 */
+	private int numItems;
+
+	/**
+	 * Default Constructor creates a default array size of 3, each element initialised to 0.0f
+	 */
+	public Numbers() {
+		numbers = new Float[3];
+		for(int i = 0; i<numbers.length;i++) {
+			numbers[i]=0.0f;
+		}
+		numItems = 0;
+	}
+
+	/**
+	 * Constructor that initialises the numbers array.
+	 * @param size - Max size of the numbers array
+	 */
+	public Numbers(int size) {
+		//creates array with size of given value, each element initialised to 0.0f
+		numbers = new Float[size];
+		for(int i = 0; i<numbers.length;i++) {
+			numbers[i]=0.0f;
+		}
+		numItems = 0;
+	}
+	
+	/**
+	 * Adds a value in the array
+	 * @param keyboard - Scanner object to use for input
+	 */
+	public void addValue(Scanner keyboard) {
+		//if the array is full, message is displayed
+		if(numbers.length == numItems) {
+			System.out.println("Array full");
+		}else {
+		//value is added to array
+		System.out.println("Enter value:");
+		Float value = keyboard.nextFloat();
+		numbers[numItems] = value;
+		numItems+=1;
+		}
+	}
+	
+	/**
+	 * Calculates the average of all the values in the numbers array.
+	 * @return float value that represents the average
+	 */
+	public float calcAverage() {
+		//creates a total of all array elements
+		float sum = 0.0f;
+		for(int i=0; i<numItems;i++) {
+			sum += numbers[i];
+		}
+		
+		//divides sum of all elements by the size of the array to get the mean average
+		float average = sum/numbers.length;
+		
+		//returns average
+		return (float) average;
+	}
+	/**
+	 * Prints the numbers on the array as a string
+	 */
+	@Override
+	public String toString() {
+		//StringBuilder class creates a string of all elements in array, except for if the element is 0.0f
+		StringBuilder builder = new StringBuilder();
+		for(int i=0; i<numbers.length;i++) {
+			if(numbers[i] != 0.0f) {
+				builder.append(numbers[i] + "\n");
+			}
+		}
+		//joined string is created
+		String joined = builder.toString();
+		//joined string returned
+		return joined;
+	}
+	/**
+	 * returns the average of elements in array, the minimum and maximum values in the array, and also returns modulo between those values.
+	 */
+	public void findMinMax() {
+		//average is printed first
+		System.out.print("Average is: " + calcAverage());
+		
+		//array is sorted from lowest to highest, minimum is first element, highest is last element
+		Arrays.sort(numbers);
+		float min = numbers[0];
+		float max = numbers[numbers.length-1];
+		
+		//finding modelo for max and min
+		float maxModMin = max % min;
+		
+		System.out.print(", Minimum value is: " + min + ", Maximum value is: " + max + ", max mod min is: " + maxModMin + "\n\n");
+		
+	}
+	
+}
